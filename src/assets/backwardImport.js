93bf98e94cbda4bd0cf5cec24d35ce6ceacd5dac
Skylar Saveland <skylar.saveland@jpmchase.com>
2016-07-10 03:50:13 -0700

    es5 ...

--- src/assets/backwardImport.js
+++ src/assets/backwardImport.js
@@ -14,7 +14,7 @@ module.exports = function backwardImport(raw) {
         imports = matches.map(function(str){
             var path = str.replace(/\s+/g, '').replace(/<-/, '');
             var name = normalize(_.last(path.split('/'))) + _.uniqueId();
-            var regexBrackets = new RegExp(`["']` + str + `["']`,`g`);
+            var regexBrackets = new RegExp('["\']' + str + '["\']', 'g');
             raw = raw.replace(regexBrackets, name);
             return {
                 name: name,
